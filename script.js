0a5ca8ec2859e33d57ee871fde822f80f6f0baf7
crossjames934 <crossjames934@gmail.com>
2019-01-12 19:51:05 +0000

    parallax and other visual effects implemented

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-// const introPics = $("#introPics");
-//
 // $(window).resize();
+
+const mapCalc = function(n, start1, stop1, start2, stop2) {
+    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+};
+
+let scrollTop = 0;
+
+$(window).scroll(event => {
+    scrollTop = $(window).scrollTop();
+    console.log(scrollTop);
+    parallaxBackground();
+});
+
+function parallaxBackground() {
+    let bgPosition = mapCalc(scrollTop, 0, window.innerHeight * 1.5, 0, -50);
+    $("#codingBackground").css({"background-position": `0 ${bgPosition}px`})
+}
