271407bf235e13338ff63e1bd7270d569564103f
crossjames934 <crossjames934@gmail.com>
2019-01-14 19:47:05 +0000

    fleshing out content

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Calculates the proportions of a variable's value between two limits to another pair of limits
 const mapCalc = (n, start1, stop1, start2, stop2) => ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
 
+// DOM elements got using JQuery
+const introSection = $("#introFullPage");
+const gameSection = $("#gamesFullPage");
+const backgroundGap = $("#codingBackground");
+
 // Determines what to show on the left / top based on where the user has scrolled to
 let leftSection = 0;
 
@@ -27,20 +32,23 @@ function scrollHandler() {
     parallaxBackground();
     // REFACTOR THIS
     // get heights of each element and work out where the next section starts
-    if (leftSection !== 0 && scrollTop < window.innerHeight * 0.5) {
+    // Do not make these global in case window size changes
+    const gapHeight = backgroundGap.height();
+    const introHeight = introSection.height();
+    const gameHeight = gameSection.height();
+    if (leftSection !== 0 && scrollTop < gapHeight) {
+        $("#section_" + leftSection)
+            .addClass("vanish")
+            .removeClass("appear")
+            .css({"pointer-events": "none"});
+        $("#section_0")
+            .addClass("appear")
+            .removeClass("vanish")
+            .css({"pointer-events": "auto"});
         leftSection = 0;
-        $("#section_1")
-            .addClass("vanish")
-            .removeClass("appear")
-            .css({"pointer-events": "none"});
-        $("#section_0")
-            .addClass("appear")
-            .removeClass("vanish")
-            .css({"pointer-events": "auto"});
     }
-    if (leftSection !== 1 && scrollTop > window.innerHeight) {
-        leftSection = 1;
-        $("#section_0")
+    if (leftSection !== 1 && scrollTop > introHeight && scrollTop < introHeight + gameHeight) {
+        $("#section_" + leftSection)
             .addClass("vanish")
             .removeClass("appear")
             .css({"pointer-events": "none"});
@@ -48,6 +56,18 @@ function scrollHandler() {
             .addClass("appear")
             .removeClass("vanish")
             .css({"pointer-events": "auto"});
+        leftSection = 1;
+    }
+    if (leftSection !== 2 && scrollTop > introHeight + gameHeight + gapHeight) {
+        $("#section_" + leftSection)
+            .addClass("vanish")
+            .removeClass("appear")
+            .css({"pointer-events": "none"});
+        $("#section_2")
+            .addClass("appear")
+            .removeClass("vanish")
+            .css({"pointer-events": "auto"});
+        leftSection = 2;
     }
 }
 
