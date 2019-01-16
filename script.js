aa714ee911ea3d967ce88e30e57c421f4dbcc326
crossjames934 <crossjames934@gmail.com>
2019-01-16 19:39:45 +0000

    initial page all sections added

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ const mapCalc = (n, start1, stop1, start2, stop2) => ((n-start1)/(stop1-start1))
 // DOM elements got using JQuery
 const introSection = $("#introFullPage");
 const gameSection = $("#gamesFullPage");
+const frontEndSection = $("#standaloneFullPage");
+const fullStackSection = $("#fullStackFullPage");
+const dataVisSection = $("#dataVisFullPage");
+const artSection = $("#artFullPage");
 const backgroundGap = $("#codingBackground");
+
+// Reset these values if window resized
+let gapHeight = backgroundGap.outerHeight(true);
+let introHeight = introSection.outerHeight(true);
+let gameHeight = gameSection.outerHeight(true);
+let frontEndHeight = frontEndSection.outerHeight(true);
+let fullStackHeight = fullStackSection.outerHeight(true);
+let dataVisHeight = dataVisSection.outerHeight(true);
+let artHeight = artSection.outerHeight(true);
 
 // Determines what to show on the left / top based on where the user has scrolled to
 let leftSection = 0;
@@ -14,66 +27,71 @@ let scrollTop = 0;
 let scrolling = false;
 
 // Scrolling events
-$(window).scroll(() => {
-    scrolling = true;
-});
+$(window)
+    .scroll(() => { scrolling = true; })
+    .resize(() => { setTimeout(grabNewHeights, 100) });
 
 // Interval to check whether user is scrolling, to run function below it
 setInterval(() => {
     if (scrolling) {
         scrollHandler();
-        scolling = false;
+        scrolling = false;
     }
-}, 10);
+}, 50);
+
+// Window Resized
+function grabNewHeights() {
+    gapHeight = backgroundGap.outerHeight(true);
+    introHeight = introSection.outerHeight(true);
+    gameHeight = gameSection.outerHeight(true);
+    frontEndHeight = frontEndSection.outerHeight(true);
+    fullStackHeight = fullStackSection.outerHeight(true);
+    dataVisHeight = dataVisSection.outerHeight(true);
+    artHeight = artSection.outerHeight(true);
+}
 
 // Only call this every few milliseconds as not to slow down performance
 function scrollHandler() {
     scrollTop = $(window).scrollTop();
     parallaxBackground();
-    // REFACTOR THIS
-    // get heights of each element and work out where the next section starts
-    // Do not make these global in case window size changes
-    const gapHeight = backgroundGap.height();
-    const introHeight = introSection.height();
-    const gameHeight = gameSection.height();
-    if (leftSection !== 0 && scrollTop < gapHeight) {
-        $("#section_" + leftSection)
-            .addClass("vanish")
-            .removeClass("appear")
-            .css({"pointer-events": "none"});
-        $("#section_0")
-            .addClass("appear")
-            .removeClass("vanish")
-            .css({"pointer-events": "auto"});
-        leftSection = 0;
-    }
-    if (leftSection !== 1 && scrollTop > introHeight && scrollTop < introHeight + gameHeight) {
-        $("#section_" + leftSection)
-            .addClass("vanish")
-            .removeClass("appear")
-            .css({"pointer-events": "none"});
-        $("#section_1")
-            .addClass("appear")
-            .removeClass("vanish")
-            .css({"pointer-events": "auto"});
-        leftSection = 1;
-    }
-    if (leftSection !== 2 && scrollTop > introHeight + gameHeight + gapHeight) {
-        $("#section_" + leftSection)
-            .addClass("vanish")
-            .removeClass("appear")
-            .css({"pointer-events": "none"});
-        $("#section_2")
-            .addClass("appear")
-            .removeClass("vanish")
-            .css({"pointer-events": "auto"});
-        leftSection = 2;
+    // Sections array - used to refactor previous code
+    // The array contains sub-arrays with two element determining different scroll limits
+    // The first value [0] in each sub-array determines where the content will change when the user scrolls DOWN
+    // The second value [1] determines where the content will switch back when the user scrolls UP
+    const sections = [
+        [0,
+            gapHeight],
+        [introHeight,
+            introHeight + gameHeight],
+        [introHeight + gapHeight + gameHeight,
+            introHeight + gameHeight + frontEndHeight + gapHeight],
+        [introHeight + gameHeight + frontEndHeight + gapHeight * 2,
+            introHeight + gameHeight + frontEndHeight + fullStackHeight + gapHeight * 2],
+        [introHeight + gameHeight + frontEndHeight + fullStackHeight + gapHeight * 3,
+            introHeight + gameHeight + frontEndHeight + fullStackHeight + dataVisHeight + gapHeight * 3],
+        [introHeight + gameHeight + frontEndHeight + fullStackHeight + dataVisHeight + gapHeight * 4,
+            introHeight + gameHeight + frontEndHeight + fullStackHeight + dataVisHeight + artHeight + gapHeight * 4],
+        [introHeight + gameHeight + frontEndHeight + fullStackHeight + dataVisHeight + artHeight + gapHeight * 4,
+            Infinity]
+    ];
+    for (let i = 0; i < sections.length; i++) {
+        if (leftSection !== i && scrollTop > sections[i][0] && scrollTop < sections[i][1]) {
+            $("#section_" + leftSection)
+                .addClass("vanish")
+                .removeClass("appear")
+                .css({"pointer-events": "none"});
+            $("#section_" + i)
+                .addClass("appear")
+                .removeClass("vanish")
+                .css({"pointer-events": "auto"});
+            leftSection = i;
+        }
     }
 }
 
 // Background parallax - move at different speed to scrolling
 function parallaxBackground() {
-    let bgPosition = mapCalc(scrollTop, 0, window.innerHeight * 1.5, 0, -100);
-    $("#codingBackground").css({"background-position": `0 ${bgPosition}px`})
+    let bgPosition = mapCalc(scrollTop, 0, window.innerHeight * 1.5, 100, -100);
+    $("#codingBackground").css({"background-position": `0 ${bgPosition}px`});
 }
 
